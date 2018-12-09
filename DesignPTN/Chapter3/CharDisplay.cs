5312df382a9797cbc41fdb652f5b3b5f70f3e300
Daichi Hirose <dhirose12345@gmail.com>
2018-12-10 06:11:36 +0900

    ３章追加（課題前）

---
+++ DesignPTN/Chapter3/CharDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3
+{
+    class CharDisplay : AbstractDisplay
+    {
+        private char _Ch;
+
+        public CharDisplay(char ch)
+        {
+            this._Ch = ch;
+        }
+
+        public override void Open()
+        {
+            Console.Write("<<");
+        }
+
+        public override void Print()
+        {
+            Console.Write(_Ch);
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine(">>");
+        }
+    }
+}
